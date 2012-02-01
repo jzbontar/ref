b1e8c4d9eeb7b1f340a15969821deef70c6a0155
Jure Zbontar <jure.zbontar@gmail.com>
2012-02-01 16:22:10 +0100

    Fixed parsed_bibtex for one author

--- ref.py
+++ ref.py
@@ -211,8 +211,7 @@ def parse_bibtex(bibtex):
     d.update(dict(re.findall(reg, bibtex, re.MULTILINE)))
     for k, v in d.items():
         d[k] = re.sub(r'[\'"{}\\=]', '', v)
-    d['author'] = ', '.join(a[:a.find(',')]
-        for a in d['author'].split(' and '))
+    d['author'] = ', '.join(a.split(',')[0] for a in d['author'].split(' and '))
     if 'journal' not in d:
         d['journal'] = d.get('booktitle', '')
     return d
