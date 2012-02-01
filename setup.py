d26de0706b7cf3ec0f81e535d3f900ead66690a7
Jure Zbontar <jure.zbontar@gmail.com>
2012-02-01 18:38:43 +0100

    Distutils setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='ref',
+    version='0.1',
+    author='Jure Zbontar',
+    author_email='jure.zbontar@gmail.com',
+    url='https://bitbucket.org/jzbontar/ref/',
+    description='A usable reference manager.',
+    py_modules=['ref', 'gui_vim'],
+    scripts=['ref'])
